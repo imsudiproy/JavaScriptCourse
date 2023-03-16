0afdcecc8e42ea44ad364a232404afcd57ff1c24
Sudip Roy <sudiproy1999@gmail.com>
2023-03-17 01:15:56 +0530

    add function in hello world

--- 001_helloWorld.js
+++ 001_helloWorld.js
@@ -1,2 +1,13 @@
-//this will print output in screen
 console.log("Hello World")
+
+//function declaration
+function sumOfThreeNumbers(a, b, c){
+  return a+b+c
+}
+
+//Directly calling the function
+console.log(sumOfThreeNumbers(5, 5, 5))
+
+//storing the function in a veriable
+d = sumOfThreeNumbers(10, 10, 10)
+console.log(d)
